fa8163841aeb36f027b8f897c8770220da69ce9b
Christian Brachert <christian.brachert@investiert.de>
2015-11-09 22:18:16 +0100

    tescht

--- alumni.py
+++ alumni.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask.ext.cors import CORS
 from decorators import crossorigin
 
@@ -7,11 +7,23 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+
 class HelloWorld(Resource):
-    def get(self):
+    def post(self):
         return {'hello': 'world'}
 
+class PutUser(Resource):
+    def post(self):
+        return 201
+        #parser = reqparse.RequestParser()
+        #parser.add_argument('email')
+        #args = parser.parse_args()
+        #task = {'task': args['mail']}
+        #print(task)
+        #return 201
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(PutUser, '/PutUser')
 
 if __name__ == '__main__':
     app.run()
