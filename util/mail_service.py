b6a1cebc830c28a707d0a059e0c1f62c67966245
Christian Brachert <christian.brachert@investiert.de>
2015-11-10 01:45:06 +0100

    string issues

--- util/mail_service.py
+++ util/mail_service.py
@@ -7,7 +7,7 @@ def send_registration_confirmation(email, code):
 
     msg = Message('Invitation from alumni network',
                   recipients=[email])
-    msg.html = 'You were invited at find.my.alumnis. Congratulations!<br><br>Please click on the following link to register<br><a href="http://localhost:5000/#/register/' + code + '">Link</a>'
+    msg.html = 'You were invited at find.my.alumnis. Congratulations!<br><br>Please click on the following link to register<br><a href="http://localhost:5000/#/register/' + str(code) + '">Link</a>'
 
     mail.send(msg)
 
