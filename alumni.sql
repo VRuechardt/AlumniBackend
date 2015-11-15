a89a454b6cc6e4e04f99eab4fc09622aa1aaae2f
Valentin Rüchardt <Valentin Rchardt>
2015-11-15 16:24:10 +0100

    new db structure

--- alumni.sql
+++ alumni.sql
@@ -1,13 +1,27 @@
-CREATE TABLE users
-       	(id INTEGER PRIMARY KEY AUTOINCREMENT     NOT NULL,
-       	email           TEXT,
-       	lastname        TEXT,
-       	firstname       TEXT,
-       	password        TEXT,
-       	authenticated   INTEGER,
-       	logincode		TEXT,
-       	authenticationcode TEXT);
+CREATE TABLE IF NOT EXISTS users
+   (id INTEGER PRIMARY KEY AUTOINCREMENT     NOT NULL,
+    email           TEXT,
+    lastname        TEXT,
+    firstname       TEXT,
+    password        TEXT,
+    authenticated   INTEGER,
+    logincode		TEXT,
+    authenticationcode TEXT);
 
-CREATE TABLE events
-		(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-		name			TEXT)
+CREATE TABLE IF NOT EXISTS events
+	 (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+		name			      TEXT,
+    startdate       INTEGER(20),
+    enddate         INTEGER(20));
+
+CREATE TABLE IF NOT EXISTS attendees
+   (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    userID          INTEGER,
+    eventID         INTEGER);
+
+CREATE TABLE IF NOT EXISTS announcements
+   (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    eventID         INTEGER,
+    userID          INTEGER,
+    comment         TEXT,
+    posted          INTEGER(20));
